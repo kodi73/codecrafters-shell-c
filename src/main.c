90936b8b3095c02eb90612806c9b4ed5a6b9759a
kodi73 <153742430+kodi73@users.noreply.github.com>
2026-05-14 19:13:14 +0530

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
-  // Flush after every printf
   setbuf(stdout, NULL);
-
   printf("$ ");
-
+  
+  char command[100];
+  fgets(command, sizeof (command), stdin);
+  command[strlen(command) - 1] = '\0';
+  printf("%s: command not found", command);
   return 0;
 }
